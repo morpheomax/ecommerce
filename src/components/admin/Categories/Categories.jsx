0117f099c44b45f9fe27c5e505b76d5bcf0c9eb3
Max Sandoval <68491989+morpheomax@users.noreply.github.com>
2023-08-31 21:29:12 -0400

    Actualización CRUD Categorias

--- src/components/admin/Categories/Categories.jsx
+++ src/components/admin/Categories/Categories.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Button, Form, Alert, Row, Col, Container } from "react-bootstrap";
 import axiosInstance from "../../../config/axios";
+import { UserContext } from "../../../context/user/userContext";
 import { Table } from "react-bootstrap";
 
 const initialForm = {
@@ -13,10 +14,17 @@ const initialForm = {
 
 export const Categories = () => {
   const token = JSON.parse(localStorage.getItem("loginFormData"));
-  const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [formData, setFormData] = useState(initialForm);
+  const [, dispatch] = useContext(UserContext); // Obtener el contexto del usuario para manejar el estado global de autenticación
 
+  // Declarar un estado adicional para el elemento seleccionado
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  const [refresh, setRefresh] = useState(null);
+
+  // Cargar la lista de categorías al cargar la página
   useEffect(() => {
     axiosInstance
       .get("/categories/")
@@ -32,44 +40,105 @@ export const Categories = () => {
         console.error("Error al obtener la lista de Categorías:", error);
         setIsLoading(false);
       });
-  }, []);
+  }, [refresh]);
 
+  // Función para crear una nueva categoría
+  const handleCreateCategory = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      // Realizar la solicitud POST para crear una categoría
+      const { data } = await axiosInstance.post(
+        "/categories",
+        formData,
+
+        {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Token ${token}`,
+          },
+        }
+      );
+
+      // Agregar la nueva categoría a la lista
+
+      setFormData(initialForm);
+      setIsLoading(false);
+      setRefresh(data);
+    } catch (error) {
+      setIsLoading(false);
+      console.error("Error al registrar categoría:", error);
+    }
+  };
+
+  // Función para actualizar una categoría
+  const handleUpdateCategory = async (categoryId) => {
+    try {
+      console.log(categoryId);
+      // Realizar la solicitud PUT para actualizar una categoría
+      const { data } = await axiosInstance.put(
+        `/categories/${categoryId}`,
+        formData, // Enviar formData en el cuerpo de la solicitud
+        {
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Token ${token}`,
+          },
+        }
+      );
+
+      // Actualizar la lista de categorías con la categoría actualizada
+      setCategories((prevCategories) =>
+        prevCategories.map((category) =>
+          category._id === data._id ? data : category
+        )
+      );
+
+      console.log(data);
+      console.log(data._id);
+      // Limpiar el formulario
+      setFormData(initialForm);
+      // Restablecer el valor de isLoading (si es necesario)
+      setIsLoading(false);
+      // Puedes omitir esta línea si no necesitas refrescar la página
+      setRefresh(data);
+    } catch (error) {
+      // Manejar errores aquí
+      setIsLoading(false);
+      console.error("Error al actualizar la categoría:", error);
+    }
+  };
+
+  // Función para manejar el botón "Editar"
+  const handleEditClick = (category) => {
+    setSelectedCategory(category);
+    // Llenar el formulario con los datos del elemento seleccionado
+    setFormData({
+      name: category.name,
+      description: category.description,
+      subcategory: category.subcategory,
+    });
+  };
+
+  // Funcion para eliminar registro
   const handleDeleteCategory = async (categoryId) => {
     try {
+      // Realizar la solicitud DELETE para eliminar una categoría
       await axiosInstance.delete(`/categories/${categoryId}`, {
-        headers: { Authorization: `Token ${token}` },
+        headers: { authorization: `Token ${token}` },
       });
 
+      // Eliminar la categoría de la lista
       setCategories((prevCategories) =>
-        prevCategories.filter((category) => category._id !== categoryId)
+        prevCategories.filter((categories) => categories._id !== categoryId)
       );
     } catch (error) {
       console.error("Error al eliminar la categoría:", error);
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    try {
-      const token = JSON.parse(localStorage.getItem("loginFormData"));
-      console.log(token);
-
-      const { data } = await axiosInstance.post("/categories/", formData, {
-        headers: {
-          "Content-Type": "application/json",
-          authorization: `Token ${token}`,
-        },
-      });
-
-      setFormData(initialForm);
-      setIsLoading(false);
-    } catch (error) {
-      setIsLoading(false);
-      console.error("Error al registrar categoría:", error);
-    }
-  };
+  // Manejar cambios en el formulario
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -81,89 +150,45 @@ export const Categories = () => {
 
   return (
     <>
-      <Container fluid className="mt-4">
-        <div className="card p-4 shadow">
-          <h5 className="mb-4 text-center">Registro de Categorías</h5>
+      <div>
+        <h2>Crear Nueva Categoría</h2>
+        <Form>
+          <Form.Group>
+            <Form.Label>Nombre:</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Descripción:</Form.Label>
+            <Form.Control
+              type="text"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Button onClick={handleCreateCategory}>Crear</Button>
+          <Button onClick={() => handleUpdateCategory(selectedCategory._id)}>
+            Actualizar
+          </Button>
+        </Form>
 
-          <Form onSubmit={handleSubmit}>
-            <Row>
-              <Col xs={12} md={2}>
-                <Form.Group controlId="name">
-                  <Form.Label>Categoría</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                  />
-                </Form.Group>
-              </Col>
-              <Col xs={12} md={2}>
-                <Form.Group controlId="description">
-                  <Form.Label>Descripción</Form.Label>
-                  <Form.Control
-                    type="text"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleChange}
-                  />
-                </Form.Group>
-              </Col>
-            </Row>
-            <Row className="m-2">
-              <Button
-                type="submit"
-                disabled={isLoading}
-                variant="primary"
-                className="mb-3"
-              >
-                {isLoading ? "Guardando..." : "Crear"}
-              </Button>
-            </Row>
-          </Form>
-        </div>
-      </Container>
-
-      <div className="m-5">
         <h2>Lista de Categorías</h2>
-        {isLoading ? (
-          <p>Cargando Categorías...</p>
-        ) : (
-          <>
-            <h3>Categorías</h3>
-            <Table striped bordered hover responsive>
-              <thead>
-                <tr>
-                  <th>Categoría</th>
-                  <th>Descripción</th>
-                  <th>Sub Categoría</th>
-                  <th>Descripción Sub</th>
-                  <th>Acciones</th>
-                </tr>
-              </thead>
-              <tbody>
-                {categories.map((category) => (
-                  <tr key={category._id}>
-                    <td>{category.name}</td>
-                    <td>{category.description}</td>
-                    <td>{category.subcategory}</td>
-                    <td>{category.subdescription}</td>
-                    <td>
-                      <div className="d-flex">
-                        <div
-                          onClick={() => handleDeleteCategory(category._id)}
-                          className="text-danger"
-                        >
-                          <i className="bi bi-trash3"></i>
-                        </div>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </>
-        )}
+        <ul>
+          {categories?.map((category) => (
+            <li key={category._id}>
+              {category.name} - {category.description}
+              <button onClick={() => handleEditClick(category)}>Editar</button>
+              <button onClick={() => handleDeleteCategory(category._id)}>
+                Eliminar
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
